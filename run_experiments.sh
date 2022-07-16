f42cd53b2fe5e9efdd3cb4c73809ce605f051ddf
Naoki <morihira3513@gmail.com>
2022-07-17 00:13:57 +0900

    modified bash script

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,2 +1,10 @@
-python spinup/algos/pytorch/td3/td3.py --env Pendulum-v0 --recurrent True --device cuda
-python spinup/algos/pytorch/td3/td3.py --env Pendulum-v0 --recurrent False --device cuda
+
+for ((i=0;i<6;i+=1))
+do
+  python spinup/algos/pytorch/td3/td3.py --env Pendulum-v0 --seed=$i --recurrent True --device cuda
+done
+
+for ((i=5;i<11;i+=1))
+do
+  python spinup/algos/pytorch/td3/td3.py --env Pendulum-v0 --seed=$i --recurrent False --device cuda
+done
